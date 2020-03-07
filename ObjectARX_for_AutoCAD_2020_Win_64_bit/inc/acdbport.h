e7629cdc25f6f216db36a358bc6283db32eb5f51
Neil Jackson <neil@rattnow.com>
2020-03-06 16:19:13 -0800

    copied the EllipseJig sample project from the AutoCAD SDK as a starting point (changed filenames to match the name of this project).  copied the include files from the SDK.

---
+++ ObjectARX_for_AutoCAD_2020_Win_64_bit/inc/acdbport.h
@@ -0,0 +1,20 @@
+//
+//////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright 2019 Autodesk, Inc.  All rights reserved.
+//
+//  Use of this software is subject to the terms of the Autodesk license 
+//  agreement provided at the time of installation or download, or which 
+//  otherwise accompanies this software in either electronic or hard copy form.   
+//
+//////////////////////////////////////////////////////////////////////////////
+//
+#pragma once
+
+#ifdef  ACDB_API
+    #include "adesk.h"
+    #define   ACDB_PORT ADESK_EXPORT
+#else
+    #define   ACDB_PORT
+#endif
+
